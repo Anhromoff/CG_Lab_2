c8a7e75d81c779978ec79fc45d0b45fc9513ff72
anhromoff <andrey@hromov.com>
2018-04-24 18:54:39 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Media3D;
+
+namespace CG_Lab_2
+{
+    public partial class MainWindow : Window
+    {
+        Wedge w; // клин
+        PerspectiveCamera pcam; // изометрическая проекция
+        OrthographicCamera ocam; // ортогональная проекция
+        const int MARGIN = 15;
+
+        // Инициализация окна
+        public MainWindow()
+        {
+            InitializeComponent();
+            pcam = new PerspectiveCamera();
+            ocam = new OrthographicCamera();
+        }
+
+        // Отрисовка клина
+        public void DrawWedge()
+        {
+            if((textbox_a != null) && (textbox_b != null) && (textbox_c != null) && (textbox_d != null))
+            {
+                // считываем стороны клина
+                double a = Convert.ToDouble(textbox_a.Text);
+                double b = Convert.ToDouble(textbox_b.Text);
+                double c = Convert.ToDouble(textbox_c.Text);
+                double d = Convert.ToDouble(textbox_d.Text);
+
+                // Вызываем конструктор и функцию отрисовки
+                w = new Wedge(a, b, c, d);
+                w.Draw(geometry_model_3d);
+
+                // Располагаем камеру в зависимости от размеров изображения
+                pcam.Position = new Point3D(b + MARGIN, a + MARGIN, d + MARGIN);
+                pcam.LookDirection = new Vector3D(-b - MARGIN, -a - MARGIN, -d - MARGIN);
+
+                ocam.Position = new Point3D(b + MARGIN, a + MARGIN, d + MARGIN);
+                ocam.LookDirection = new Vector3D(-b - MARGIN*5, -a - MARGIN*5, -d - MARGIN*5);
+                ocam.Width = 20;
+            }
+        }
+
+        // Обработчики нажатия
+        private void Build_perspective_Click(object sender, RoutedEventArgs e)
+        {
+            DrawWedge();
+            my_viewport.Camera = pcam;
+        }
+
+        private void Build_ortogonal_Click(object sender, RoutedEventArgs e)
+        {
+            DrawWedge();
+            my_viewport.Camera = ocam;
+        }
+    }
+}
---
+++ Wedge.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Media.Media3D;
+
+namespace CG_Lab_2
+{
+    class Wedge
+    {
+        private Point3D A, B, C, A1, B1, C1;
+
+        // Просчитываем каждую точку клина
+        public Wedge(double a, double b, double c, double d)
+        {
+            double x = (a * a + b * b - c * c) / (2 * a);
+            double y = Math.Sqrt(b * b - x * x);
+
+            A = new Point3D(0, 0, 0);
+            B = new Point3D(a, 0, 0);
+            C = new Point3D(x, y, 0);
+
+            A1 = new Point3D(0, 0, d);
+            B1 = new Point3D(a, 0, d);
+            C1 = new Point3D(x, y, d);
+        }
+
+        // Отрисовка
+        public void Draw(GeometryModel3D geometry_model_3d)
+        {
+            MeshGeometry3D mesh = new MeshGeometry3D();
+
+            // Заносим все точки в массив
+            mesh.Positions.Add(A);
+            mesh.Positions.Add(B);
+            mesh.Positions.Add(C);
+            mesh.Positions.Add(A1);
+            mesh.Positions.Add(B1);
+            mesh.Positions.Add(C1);
+
+            // По очереди для каждого теугольника пишем порядок соединения его вершин линиями
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(1);
+            mesh.TriangleIndices.Add(0);
+
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(4);
+            mesh.TriangleIndices.Add(5);
+
+            mesh.TriangleIndices.Add(1);
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(0);
+
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(1);
+            mesh.TriangleIndices.Add(4);
+
+            mesh.TriangleIndices.Add(0);
+            mesh.TriangleIndices.Add(3);
+            mesh.TriangleIndices.Add(2);
+
+            mesh.TriangleIndices.Add(5);
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(3);
+
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(4);
+            mesh.TriangleIndices.Add(1);
+
+            mesh.TriangleIndices.Add(4);
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(5);
+
+            geometry_model_3d.Geometry = mesh;
+        }
+    }
+}
